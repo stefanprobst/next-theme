1a75b11f4bf8c12c3921b7a7e0bbd84d032529cf
Stefan Probst <stefanprobst@posteo.de>
2022-02-21 09:07:56 +0100

    fix: next/script does not support beforeInteractive strategy on inline scripts, move to _document

--- src/InitialThemeScript.tsx
+++ src/InitialThemeScript.tsx
@@ -1,20 +1,8 @@
-import HeadModule from 'next/head.js'
-import ScriptModule from 'next/script.js'
 import { Fragment } from 'react'
 
 import { dataAttributeTheme, localStorageKeyTheme, styleElementId, themes } from './theme.config.js'
 
-/**
- * Nwxt.js only provides CommonJs exports.
- *
- * @see https://github.com/vercel/next.js/issues/30402
- */
-/* @ts-expect-error CommonJS module. */
-const { default: Head } = HeadModule
-/* @ts-expect-error CommonJS module. */
-const { default: Script } = ScriptModule
-
-const setInitialThemeScript = `const theme = localStorage.getItem('${localStorageKeyTheme}')
+export const setInitialThemeScript = `const theme = localStorage.getItem('${localStorageKeyTheme}')
 if (
   theme === '${themes.dark}' ||
   (theme == null && matchMedia('(prefers-color-scheme: ${themes.dark})').matches)
@@ -28,14 +16,8 @@ if (
 export function InitialThemeScript(): JSX.Element {
   return (
     <Fragment>
-      <Script
-        id="set-initial-theme"
-        dangerouslySetInnerHTML={{ __html: setInitialThemeScript }}
-        strategy="beforeInteractive"
-      />
-      <Head>
-        <style id={styleElementId} />
-      </Head>
+      <script id="set-initial-theme" dangerouslySetInnerHTML={{ __html: setInitialThemeScript }} />
+      <style id={styleElementId} />
     </Fragment>
   )
 }
